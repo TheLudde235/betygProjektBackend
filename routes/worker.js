374aae7afafc00c406352a26cdba34b325fbda88
TheLudde235 <ludde.lagerstrom@outlook.com>
2023-03-09 12:05:09 +0100

    added response for registering worker

--- routes/worker.js
+++ routes/worker.js
@@ -40,6 +40,7 @@ export const registerWorker = async (req, res) => {
       html: `
       <h3>Type this in the browser: ${uuid}</h3>`
     });
+    res.status(StatusCodes.OK).json({msg: 'email sent'});
   } catch (err) {
     return res.status(StatusCodes.BAD_REQUEST).json({msg: err.message});
   }
